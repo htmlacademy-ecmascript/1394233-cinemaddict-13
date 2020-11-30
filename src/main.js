63ba52c990d125307875b4741042a25a69e2f548
Vafin Artem <a.vafin85@gmail.com>
2020-11-30 22:24:13 +0500

    Рефакторинг и добавление функций

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import {getRandomInteger} from "./utils.js";
+import {getRandomInteger, renderTemplate} from "./utils.js";
 import {createUserRangTemplate} from "./view/user-rang.js";
 import {createSiteMenuTemplate} from "./view/site-menu.js";
 import {createFilterTemplate} from "./view/filter.js";
@@ -29,10 +29,6 @@ const filters = generateFilter(films);
 const sortByRating = (items) => items.slice().sort((a, b) => b.rating - a.rating);
 const sortByComments = (items) => items.slice().sort((a, b) => b.comments.length - a.comments.length);
 
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
-
 for (let film of films) {
   film.comments = new Array(getRandomInteger(ComentsAmmount.MIN, ComentsAmmount.MAX)).fill(``).map(generateRandomComment);
 }
@@ -42,14 +38,14 @@ const siteMainNode = document.querySelector(`.main`);
 const siteFooterNode = document.querySelector(`.footer`);
 const statisticNode = siteFooterNode.querySelector(`.footer__statistics`);
 
-render(siteHeaderNode, createUserRangTemplate(), `beforeend`);
-render(siteMainNode, createSiteMenuTemplate(filters), `beforeend`);
-render(siteMainNode, createFilterTemplate(), `beforeend`);
-render(siteMainNode, createMainContentTemplate(), `beforeend`);
+renderTemplate(siteHeaderNode, createUserRangTemplate(), `beforeend`);
+renderTemplate(siteMainNode, createSiteMenuTemplate(filters), `beforeend`);
+renderTemplate(siteMainNode, createFilterTemplate(), `beforeend`);
+renderTemplate(siteMainNode, createMainContentTemplate(), `beforeend`);
 
 const filmsNode = siteMainNode.querySelector(`.films`);
 
-render(filmsNode, createFilmsListTemplate(), `beforeend`);
+renderTemplate(filmsNode, createFilmsListTemplate(), `beforeend`);
 
 const filmsListNode = filmsNode.querySelector(`.films-list`);
 const filmsContainerNode = filmsNode.querySelector(`.films-list__container`);
@@ -57,7 +53,7 @@ const filmsContainerNode = filmsNode.querySelector(`.films-list__container`);
 if (films.length > FILMS_AMOUNT_PER_STEP) {
   let renderedFilmCount = FILMS_AMOUNT_PER_STEP;
 
-  render(filmsListNode, createShowMoreBtnTemplate(), `beforeend`);
+  renderTemplate(filmsListNode, createShowMoreBtnTemplate(), `beforeend`);
 
   const showMoreButton = filmsListNode.querySelector(`.films-list__show-more`);
 
@@ -65,7 +61,7 @@ if (films.length > FILMS_AMOUNT_PER_STEP) {
     evt.preventDefault();
 
     films.slice(renderedFilmCount, renderedFilmCount + FILMS_AMOUNT_PER_STEP)
-    .forEach((filmsElems) => render(filmsContainerNode, createCardFilmTemplate(filmsElems), `beforeend`));
+    .forEach((filmsElems) => renderTemplate(filmsContainerNode, createCardFilmTemplate(filmsElems), `beforeend`));
 
     renderedFilmCount += FILMS_AMOUNT_PER_STEP;
 
@@ -76,11 +72,11 @@ if (films.length > FILMS_AMOUNT_PER_STEP) {
 }
 
 for (let i = 1; i < Math.min(films.length, FILMS_AMOUNT_PER_STEP + 1); i++) {
-  render(filmsContainerNode, createCardFilmTemplate(films[i]), `beforeend`);
+  renderTemplate(filmsContainerNode, createCardFilmTemplate(films[i]), `beforeend`);
 }
 
-render(filmsNode, createTopRatedListTemplate(), `beforeend`);
-render(filmsNode, createMostCommentedListTemplate(), `beforeend`);
+renderTemplate(filmsNode, createTopRatedListTemplate(), `beforeend`);
+renderTemplate(filmsNode, createMostCommentedListTemplate(), `beforeend`);
 
 const topRatedFilmsNode = filmsNode.querySelector(`.films-list--top-rated`);
 const topRatedFilmsContainerNode = topRatedFilmsNode.querySelector(`.films-list__container`);
@@ -88,14 +84,14 @@ const mostCommentedFilmsNode = filmsNode.querySelector(`.films-list--most-commen
 const mostCommentedFilmsContainerNode = mostCommentedFilmsNode.querySelector(`.films-list__container`);
 
 sortByComments(films).slice(0, 2).forEach((film) => {
-  render(mostCommentedFilmsContainerNode, createCardFilmTemplate(film), `beforeend`);
+  renderTemplate(mostCommentedFilmsContainerNode, createCardFilmTemplate(film), `beforeend`);
 });
 
 sortByRating(films).slice(0, 2).forEach((film) => {
-  render(topRatedFilmsContainerNode, createCardFilmTemplate(film), `beforeend`);
+  renderTemplate(topRatedFilmsContainerNode, createCardFilmTemplate(film), `beforeend`);
 });
 
-render(statisticNode, createSiteStatisticTemplate(films.length), `beforeend`);
+renderTemplate(statisticNode, createSiteStatisticTemplate(films.length), `beforeend`);
 
-render(siteFooterNode, createPopupTemplate(films[0]), `afterend`);
+renderTemplate(siteFooterNode, createPopupTemplate(films[0]), `afterend`);
 
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,8 @@
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
+};
+
 export const getRandomInteger = (a = 0, b = 1) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
@@ -20,4 +25,26 @@ export const generateRandomItem = (array) => {
   const randomIndex = getRandomInteger(0, array.length - 1);
 
   return array[randomIndex];
+};
+
+export const renderElement = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+  }
+};
+
+export const renderTemplate = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`); // 1
+  newElement.innerHTML = template; // 2
+
+  return newElement.firstChild; // 3
 };
